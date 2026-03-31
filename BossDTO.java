89b37ef647a4fec28a09ce71fb46785d2de91561
juYeol97 <youremail@gmail.com>
2026-03-31 16:59:06 +0900

    0331 작업내용

--- BossDTO.java
+++ BossDTO.java
@@ -5,6 +5,7 @@ public class BossDTO {
     
     private String bossType ;                   // 보스의 타입 설정                 ex) 화
     private int bossHp;                         // 보스의 HP를 설정할 bossHp        ex) 1000
+    private int bossDmg;
     private HashMap<String, Integer> bossSkill;     // 보스의 스킬과 데미지 bossSkill   ex) 몸통박치기, 100
 
 
@@ -17,13 +18,20 @@ public class BossDTO {
         this.bossType = bossType;
     }
 
-
     // 보스 Hp 게터 / 세터
     public int getBossHp() {
         return bossHp;
     }
     public void setBossHp(int bossHp) {
         this.bossHp = bossHp;
+    }
+
+    // 보스 데미지 게터 / 세터
+    public int getBossDmg() {
+        return bossDmg;
+    }
+    public void setBossDmg(int bossDmg) {
+        this.bossDmg = bossDmg;
     }
 
     // 보스 스킬 게터 / 세터
@@ -33,6 +41,7 @@ public class BossDTO {
     public void setBossSkill(HashMap<String, Integer> bossSkill) {
         this.bossSkill = bossSkill;
     }
+
     
     
 
--- setBossOptions.java
+++ setBossOptions.java
@@ -1,9 +1,9 @@
-import java.util.Map;
+import java.util.HashMap;
 
 public class setBossOptions 
 {
     BossDTO dto = new BossDTO();
-    Map<String, Integer> bossSkill;
+    HashMap<String, Integer> bossSkill;
 
 
     public BossDTO setBoss()
@@ -15,7 +15,7 @@ public class setBossOptions
         return BossDTO;
     }
 
-    
+
     // 보스 타입을 랜덤하게 설정
     private int setBossType()
     {
@@ -27,28 +27,46 @@ public class setBossOptions
         return bossType;
     }
     
-    private int bossStatus(int bossType)
+    // 보스 한마리의 
+    private BossDTO bossStatus(int bossType)
     {
         
         if(bossType == 0)
         {
             
             dto.setBossType("fire");
-            dto.setBossHp(1000);
+            dto.setBossHp(1500);
             bossSkill.put("물어뜯기", 150);
             bossSkill.put("화염방사", 200);
             bossSkill.put("초고열용광로", 250);
             bossSkill.put("분노", 0);
         }
 
+        if(bossType == 1)
+        {
+            dto.setBossType("water");
+            dto.setBossHp(2000);
+            dto.setBossSkill("ㅁㄹ",12);
+            bossSkill.put("물대포", 170);
+            bossSkill.put("깨물기", 150);
+            bossSkill.put("하이드로펌프", 230);
+            bossSkill.put("난동", 200);
+
+        }
+
+        if(bossType == 2)
+        {
+            dto.setBossType("nature");
+            dto.setBossHp(1700);
+            bossSkill.put("잎날가르기", 180);
+            bossSkill.put("광합성", 0);
+            bossSkill.put("솔라빔", 300);
+            bossSkill.put("박치기", 130);
+        }
 
 
 
-
-
-
-
-        return 0;
+        return 
     }
 
 
