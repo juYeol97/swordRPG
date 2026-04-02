49e41a9d01fb60b4f1e4eeb2f8aa25bdc63e875c
juYeol97 <youremail@gmail.com>
2026-04-02 17:09:45 +0900

    0402작업내용

--- Process.java
+++ Process.java
@@ -3,8 +3,7 @@ public class Process {
     static void run(){
 
 
-         int bossType = (int)(Math.random() * 3);
-         System.err.println(bossType);
+        
         
         //do
         //{
--- setBossOptions.java
+++ setBossOptions.java
@@ -1,57 +1,47 @@
 import java.util.HashMap;
 
-public class setBossOptions 
+public class SetBossOptions 
 {
-    BossDTO dto = new BossDTO();
-    HashMap<String, Integer> bossSkill;
-
-
-    public BossDTO setBoss()
+    
+    // 생성자
+    public SetBossOptions()
     {
-        
-        
-        int bossType;
-
-        return BossDTO;
+        setBossStatus();    // 설정된 보스 1마리 리턴
     }
-
-
-    // 보스 타입을 랜덤하게 설정
-    private int setBossType()
+ 
+    // 보스 한마리의 타입과 스테이터스를 설정
+    private BossDTO setBossStatus()
     {
+        
+        // 보스 한마리 객체 DTO, 보스 스킬을 담을 자료구조
+        BossDTO dto = new BossDTO();
+        HashMap<String, Integer> bossSkill = null ;
+
 
         // 0번 화속성, 1번 수속성, 2번 풀속성
         int bossType = (int)(Math.random() * 3);
         
-        // 0, 1, 2 중 반환
-        return bossType;
-    }
-    
-    // 보스 한마리의 
-    private BossDTO bossStatus(int bossType)
-    {
-        
+        // 보스 타입에 따른 스테이터스 설정
         if(bossType == 0)
         {
-            
             dto.setBossType("fire");
             dto.setBossHp(1500);
             bossSkill.put("물어뜯기", 150);
             bossSkill.put("화염방사", 200);
             bossSkill.put("초고열용광로", 250);
             bossSkill.put("분노", 0);
+            dto.setBossSkill(bossSkill);
         }
 
         if(bossType == 1)
         {
             dto.setBossType("water");
             dto.setBossHp(2000);
-            dto.setBossSkill("ㅁㄹ",12);
             bossSkill.put("물대포", 170);
             bossSkill.put("깨물기", 150);
             bossSkill.put("하이드로펌프", 230);
             bossSkill.put("난동", 200);
-
+            dto.setBossSkill(bossSkill);
         }
 
         if(bossType == 2)
@@ -62,11 +52,11 @@ public class setBossOptions
             bossSkill.put("광합성", 0);
             bossSkill.put("솔라빔", 300);
             bossSkill.put("박치기", 130);
+            dto.setBossSkill(bossSkill);
         }
 
-
-
-        return 
+        // 보스 1마리 리턴
+        return dto;
     }
 
 
